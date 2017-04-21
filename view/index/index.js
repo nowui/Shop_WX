be3a7be90467d8990698886c5cf89dd4eef621b6
NowUI <nowui@163.com>
2017-04-21 20:13:40 +0800

    2017-04-21 20:13

---
+++ view/index/index.js
@@ -0,0 +1,33 @@
+Page({
+    data: {
+
+    },
+    onLoad: function (options) {
+        // getApp().getUserInfo(function (userInfo) {
+        //     this.setData({
+        //         userInfo: userInfo
+        //     })
+        // }.bind(this));
+    },
+    onReady: function () {
+
+    },
+    onShow: function () {
+
+    },
+    onHide: function () {
+
+    },
+    onUnload: function () {
+
+    },
+    onPullDownRefresh: function () {
+
+    },
+    onReachBottom: function () {
+
+    },
+    onShareAppMessage: function () {
+
+    }
+});
